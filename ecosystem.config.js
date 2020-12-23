a2c99c2fa1a69b9c2e40a10c86a292917d8a801e
Ninzore <33434617+Ninzore@users.noreply.github.com>
2020-12-23 20:57:27 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'BBQ',
+            name: 'caster',
             script: 'index.js',
             instances: 1,
             autorestart: true,
