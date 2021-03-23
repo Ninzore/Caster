bed96a6e7ede6ce748547219cffaf9e814e42932
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-03-23 22:46:58 +0000

    Feat: 定向翻译的对象现在会被储存为配置文件

--- plugin/translate.js
+++ plugin/translate.js
@@ -1,4 +1,5 @@
-const tencentcloud = require("tencentcloud-sdk-nodejs");
+import fs from "fs-extra";
+import {tmt} from "tencentcloud-sdk-nodejs";
 
 const TmtClient = tencentcloud.tmt.v20180321.Client;
 const CONFIG = global.config.translate.tencent;
@@ -20,6 +21,18 @@ const client = (SECRET_ID != "" && SECRET_KEY != "") ? new TmtClient(clientConfi
 
 let target = {};
 
+function init(context = false) {
+    if (fs.existsSync("data/translate.json")) {
+        fs.readJSON("data/translate.json").then(permTarget => {
+            target = permTarget;
+        });
+    }
+    else {
+        fs.writeJSON("data/translate.json", target);
+    }
+    context ? replyFunc(context, "刷新完成") : "";
+}
+
 function unescape(text) {
     return text.replace(/&amp;/g, "&").replace(/&#91;/g, "[").replace(/&#93;/g, "]")
         .replace(/&lt;/g, "<").replace(/&gt;/g, ">");
@@ -39,7 +52,7 @@ async function translate(sourceLang, targetLang, sourceText) {
         "Source": sourceLang,
         "Target": targetLang,
         "ProjectId": 0
-    }).then((res) => {
+    }).then(res => {
         return res.TargetText;
     }, (err) => {
         console.error("translate error", err);
@@ -74,6 +87,7 @@ function pointTo(context, user_id) {
     if (target[context.group_id] === undefined) target[context.group_id] = [];
     target[context.group_id].push(parseInt(user_id));
     replyFunc(context, `接下来${user_id}说的每句话都会被翻译`);
+    fs.writeJSON("data/translate.json", target);
     return;
 }
 
@@ -83,6 +97,7 @@ function unpoint(context, user_id) {
     target[context.group_id].some(aim => {return aim == user_id})) {
         target[context.group_id] = target[context.group_id].filter(id => id != user_id);
         replyFunc(context, `对${user_id}的定向翻译已停止`);
+        fs.writeJSON("data/translate.json", target);
     }
     else replyFunc(context, `${user_id}不在定向翻译列表中`);
 }
@@ -92,6 +107,7 @@ function allClear(context) {
     if (target[group_id] != undefined && target[group_id].length > 0) {
         delete target[group_id];
         replyFunc(context, "已清空本群所有目标");
+        fs.writeJSON("data/translate.json", target);
     } else {
         replyFunc(context, "本群无目标");
     }
@@ -142,9 +158,16 @@ function transEntry(context) {
             else replyFunc(context, "您配吗");
             return true;
         }
+        else if (/^刷新翻译列表$/.test(context.message)) {
+            if (!/member/.test(context.sender.role)) init(context);
+            else replyFunc(context, "您配吗");
+            return true;
+        }
         else return false;
     }
     else return false;
 }
 
-module.exports = {transEntry, orientedTrans, translate};
+init();
+
+export default {transEntry, orientedTrans, translate};
